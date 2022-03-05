fd5127412d8ac40bb381f2259fe43be35b127a09
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-03-05 00:25:10 -0600

    finish merge

---
+++ source/Notes/Support/ToolbarRegistration.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using ToolbarControl_NS;
+
+namespace Proximity
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class RegisterToolbar : MonoBehaviour
+    {
+        void Start()
+        {
+            ToolbarControl.RegisterMod(StockToolbar.MODID, StockToolbar.MODNAME);
+        }
+    }
+}
